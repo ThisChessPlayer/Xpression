2492a3030cc9370d4d7a26c2d6f9fd038c9ee5b0
Graham McKnight <graham.mcknight2@gmail.com>
2017-03-04 15:06:19 -0800

    Fix serialization

--- utility.py
+++ utility.py
@@ -3,24 +3,35 @@ import pandas as pd
 import sys
 import RekognitionInterface
 import os
+import csv
+import json
 
-def getImageInfo(image_directory, images_list, index):
+def getImageInfo(drivers_df, image_directory, images_list, index):
 
-    drivers_df = pd.read_csv(images_list)
-
-    drivers_list = []
+    #drivers_df = pd.read_csv(images_list)
 
     filename = image_directory + "/train/" + drivers_df['classname'][index] + "/"+ drivers_df['img'][index]
     print(filename)
     if (not os.path.isfile(str(filename))):
         filename = image_directory + "/test/" + drivers_df['img'][index]
     imageInfo = {}
-    imageInfo['attributes'] = TestRekognition.recognizeLabels(filename)
+    imageInfo['attributes'] = RekognitionInterface.recognizeLabels(filename)
     imageWrapper = []
     imageWrapper.append(drivers_df['subject'][index])
     imageInfo['subject'] = drivers_df['subject'][index]
     imageInfo['classname'] = drivers_df['classname'][index]
     imageInfo['imgname'] = drivers_df['img'][index]
-    imageWrapper.append(imageInfo)
+    imageWrapper.append(json.dumps(imageInfo))
     print(imageWrapper)
     return imageWrapper
+
+def makeCSV(image_directory, images_list, indexStart, indexEnd):
+    drivers_df = pd.read_csv(images_list)
+    with open("output2.csv", 'w', newline='') as outfile:
+        writer = csv.writer(outfile)
+        for i in range (indexStart, indexEnd):
+            image = getImageInfo(drivers_df, image_directory, images_list, i)
+            writer.writerow(image)
+        #['id'].append(image[0])
+        #imagedf['attributes'].append(image[1])
+    
